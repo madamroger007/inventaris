092ba499da277a6c2532b5c6b426ae79f74b2500
adam <104338817+madamroger007@users.noreply.github.com>
2025-09-07 22:29:28 +0700

    fix: mengganti nama label pada denah penyimpanan

--- app/Filament/Resources/DenahPenyimpananResource/Pages/CreateDenahPenyimpanan.php
+++ app/Filament/Resources/DenahPenyimpananResource/Pages/CreateDenahPenyimpanan.php
@@ -9,4 +9,8 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateDenahPenyimpanan extends CreateRecord
 {
     protected static string $resource = DenahPenyimpananResource::class;
+    public function getTitle(): string
+    {
+        return 'Tambah Lokasi Penyimpanan';
+    }
 }
--- app/Filament/Resources/DenahPenyimpananResource/Pages/EditDenahPenyimpanan.php
+++ app/Filament/Resources/DenahPenyimpananResource/Pages/EditDenahPenyimpanan.php
@@ -10,6 +10,11 @@ class EditDenahPenyimpanan extends EditRecord
 {
     protected static string $resource = DenahPenyimpananResource::class;
 
+    public function getTitle(): string
+    {
+        return 'Edit Lokasi Penyimpanan';
+    }
+
     protected function getHeaderActions(): array
     {
         return [
